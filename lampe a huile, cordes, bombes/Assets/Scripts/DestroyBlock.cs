db0ca6ae548888deb6648cdb02c019ed8c58c13a
takuryto2 <150046987+takuryto2@users.noreply.github.com>
2024-03-05 14:24:18 +0100

    bloc destructible

--- lampe a huile, cordes, bombes/Assets/Scripts/DestroyBlock.cs
+++ lampe a huile, cordes, bombes/Assets/Scripts/DestroyBlock.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DestroyBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void DestroyBlk()
     {
-        
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Explosion"))
+        {
+            DestroyBlk();
+        }
     }
 }
